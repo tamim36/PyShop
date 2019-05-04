3c175973794c18b4f37af03174eea619e3a60f03
tamim36 <tamimarefinanik@gmail.com>
2019-05-04 17:36:16 +0600

    creating models

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('new/', views.new)
 ]
--- products/views.py
+++ products/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request):
     return HttpResponse('Hello world')
+
+
+def new(request):
+    return HttpResponse('this is a new products page')
