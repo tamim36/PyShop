85fc81ccca8f3f74feef07c7b3271ad55e931c75
tamim36 <tamimarefinanik@gmail.com>
2019-05-05 01:39:39 +0600

    add model in admin section

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Product, Offer
 
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount', 'description')
+
+
+admin.site.register(Offer, OfferAdmin)
+
+admin.site.register(Product, ProductAdmin)
