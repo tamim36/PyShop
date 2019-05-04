03e46f9c2ecce4f44ca164a602f2ae4ea6be8cb6
tamim36 <tamimarefinanik@gmail.com>
2019-05-04 19:27:19 +0600

    connect to mySql

--- PyShop/settings.py
+++ PyShop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products.apps.ProductsConfig'
 ]
 
 MIDDLEWARE = [
@@ -73,12 +74,24 @@ WSGI_APPLICATION = 'PyShop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+#DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#   }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'PyShop',
+        'USER' : 'root',
+        'Password' : '',
+        'HOST' : 'localhost',
+        'PORT' : ''
     }
 }
+
 
 
 # Password validation
