cc095cc4d0633ebf3ab3ad29864f4ee4709c7349
tamim36 <tamimarefinanik@gmail.com>
2019-05-05 02:16:32 +0600

    using templates to show data in html

--- products/views.py
+++ products/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Hello world')
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products' : products})
 
 
 def new(request):
